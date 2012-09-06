08279ea05ac0515dbb65a7f3966a95c3cee60f94
Rico P <MailToRico@googlemail.com>
2012-09-06 10:53:03 +0200

    added math file for later use

---
+++ src/math.js
@@ -0,0 +1,172 @@
+#ifndef MATH_JS 
+#define MATH_JS 
+
+//Borrowed from gl-matrix.js 
+
+function mat4create(){}
+
+function mat4createFrom(){}
+
+function mat4set(){}
+
+function mat4equal(){}
+
+function mat4identity(){}
+
+function mat4transpose(){}
+
+function mat4determinant(){}
+
+function mat4inverse(){}
+
+function mat4toRotationMat(){}
+
+function mat4toMat3(){}
+
+function mat4toInverseMat3(){}
+
+function mat4multiply(){}
+
+function mat4multiplyVec3(){}
+
+function mat4multiplyVec4(){}
+
+function mat4translate(){}
+
+function mat4scale(){}
+
+function mat4rotate(){}
+
+function mat4rotateX(){}
+
+function mat4rotateY(){}
+
+function mat4rotateZ(){}
+
+function mat4frustum(){}
+
+function mat4perspective(){}
+
+function mat4ortho(){}
+
+function mat4lookAt(){}
+
+function mat4fromRotationTranslation(){}
+
+function mat4str(){}
+
+function vec4create(){}
+
+function vec4createFrom(){}
+
+function vec4add(){}
+
+function vec4subtract(){}
+
+function vec4multiply(){}
+
+function vec4divide(){}
+
+function vec4scale(){}
+
+function vec4set(){}
+
+function vec4equal(){}
+
+function vec4negate(){}
+
+function vec4length(){}
+
+function vec4squaredLength(){}
+
+function vec4lerp(){}
+
+function vec4str(){}
+
+function vec3create(){}
+
+function vec3createFrom(){}
+
+function vec3set(){}
+
+function vec3equal(){}
+
+function vec3add(){}
+
+function vec3subtract(){}
+
+function vec3multiply(){}
+
+function vec3negate(){}
+
+function vec3scale(){}
+
+function vec3normalize(){}
+
+function vec3cross(){}
+
+function vec3length(){}
+
+function vec3squaredLength(){}
+
+function vec3dot(){}
+
+function vec3direction(){}
+
+function vec3lerp(){}
+
+function vec3dist(){}
+
+function vec3unproject(){}
+
+function vec3rotationTo(){}
+
+function vec3str(){}
+
+function quat4create(){}
+
+function quat4createFrom(){}
+
+function quat4set(){}
+
+function quat4equal(){}
+
+function quat4identity(){}
+
+function quat4calculateW(){}
+
+function quat4dot(){}
+
+function quat4inverse(){}
+
+function quat4conjugate(){}
+
+function quat4length(){}
+
+function quat4normalize(){}
+
+function quat4add(){}
+
+function quat4multiply(){}
+
+function quat4multiplyVec3(){}
+
+function quat4scale(){}
+
+function quat4toMat3(){}
+
+function quat4toMat4(){}
+
+function quat4slerp(){}
+
+function quat4fromRotationMatrix(){}
+
+function quat4fromAxes(){}
+
+function quat4fromAngleAxis(){}
+
+function quat4toAngleAxis(){}
+
+function quat4str(){}
+
+#endif 
